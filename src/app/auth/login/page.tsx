be159e01b433398ef54f73ba9d980bdee5d2643c
Edafe Oke Great <greatedafeoke@gmail.com>
2025-05-03 09:32:53 +0100

    Added zod

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,21 +1,38 @@
-import { signIn } from '@/auth'
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { signIn } from "@/auth";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const LoginPage = (props: Props) => {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("github")
-      }}
-    >
-      <Button type="submit">Signin with GitHub</Button>
-    </form>
-  )
-}
+    <div>
+      <form
+        action={async (formData) => {
+          "use server";
+          await signIn("credentials", formData);
+        }}
+      >
+        <label>
+          Email
+          <input name="email" type="email" />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" />
+        </label>
+        <button>Sign In</button>
+      </form>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("github");
+        }}
+      >
+        <Button type="submit">Signin with GitHub</Button>
+      </form>
+    </div>
+  );
+};
 
-export default LoginPage
-
+export default LoginPage;
--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,35 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Credentials from "next-auth/providers/credentials"
+import { signInSchema } from "./lib/zod"
+import { ZodError } from "zod"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHub,
-    // Credentials({
-    //   credentials: {
-    //     email: { label: "Email", type: "email" },
-    //     password: { label: "Password", type: "password" },
-    //   },
-    // }),
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: { label: "Password", type: "password", placeholder: "Password" },
+      },
+      async authorize(credentials) {
+        try {
+          const { email, password } = await signInSchema.parseAsync(credentials)
+          if (email === "greatokedev@gmail.com" && password === "password") {
+            return { id: "1", name: "Test User" }
+          }
+          return null
+        } catch (error) {
+            if (error instanceof ZodError) {
+                console.log(error.errors)
+                // Return `null` to indicate that the credentials are invalid
+                return null
+              }
+          console.error(error)
+          return null
+        }
+      },
+    }),
   ],
   callbacks: {
     async session({ session, token }) {
